c329407efba2a118e559411b98ccb4af6f2559a8
Agustín <115501960+MenegozziAxel@users.noreply.github.com>
2023-09-29 15:30:08 -0300

    Cambios en diseño
    Le damos mas estilo a la aplicación. Aplicamos cambios en el entorno gráfico.

--- NoticiApp.py
+++ NoticiApp.py
@@ -1,15 +1,25 @@
 import tkinter as tk
 
+
 class NoticiApp: #Creamos la clase NoticiApp
     def __init__(self, ventana):
         self.ventana = ventana
         self.ventana.title("NoticiApp - Aplicación de Noticias en Tiempo Real")
         self.ventana.geometry("800x600")
 
-        self.titulo_label = tk.Label(ventana, text="Noticias en Tiempo Real - Argentina", font=("Arial", 18))
+        #Color de fondo principal
+        fondo_principal = "#C6E8E3"
+
+        self.ventana.configure(bg=fondo_principal)
+
+        #Fuente de texto personalizada
+        fuente_personalizada = ("Ginebra", 14)
+
+        self.titulo_label = tk.Label(ventana, text="Noticias en Tiempo Real - Argentina", font=("Lucida Bright", 18), bg=fondo_principal)
         self.titulo_label.pack(pady=10)
 
-        self.categoria_label = tk.Label(ventana, text="Selecciona una categoría:", font=("Arial", 14))
+        #Lista desplegable para seleccionar la categoría
+        self.categoria_label = tk.Label(ventana, text="Selecciona una categoría:", font=fuente_personalizada, bg=fondo_principal)
         self.categoria_label.pack(pady=5)
 
         categorias = ["General", "Negocios", "Entretenimiento", "Salud", "Ciencia", "Deportes", "Tecnología"] #Definimos las categorias
@@ -17,21 +27,31 @@ class NoticiApp: #Creamos la clase NoticiApp
         self.categoria_seleccionada.set(categorias[0])
 
         self.menu_categoria = tk.OptionMenu(ventana, self.categoria_seleccionada, *categorias)
+        self.menu_categoria.config(font=fuente_personalizada)
         self.menu_categoria.pack()
 
-        self.noticias_marco = tk.Frame(ventana) 
+        self.noticias_marco = tk.Frame(ventana, bg=fondo_principal)
         self.noticias_marco.pack(padx=20, pady=20, fill="both", expand=True)
 
-        self.noticias_lista = tk.Listbox(self.noticias_marco, font=("Arial", 12), selectbackground="lightblue")
+        self.noticias_lista = tk.Listbox(self.noticias_marco, selectbackground="lightblue", font=fuente_personalizada)
         self.noticias_lista.pack(fill="both", expand=True)
 
-        self.cargar_noticias_btn = tk.Button(ventana, text="Cargar Noticias", font=("Arial", 14), command=self.cargar_noticias) #Botón para cargar las noticias
+        #Estilo personalizado para botones
+        estilo_boton = {
+            "font": fuente_personalizada,
+            "bg": "#007ACC",  #Color de fondo del botón
+            "fg": "white",  #Color de texto del botón
+            "activebackground": "#005FA3",  #Color cuando se presiona el botón
+            "activeforeground": "white"  #Color del texto cuando se presiona el botón
+        }
+
+        self.cargar_noticias_btn = tk.Button(ventana, text="Cargar Noticias", command=self.cargar_noticias, **estilo_boton) #Botón para cargar las noticias
         self.cargar_noticias_btn.pack(pady=10)
 
-        self.ver_noticia_btn = tk.Button(ventana, text="Ver Noticia", font=("Arial", 14), command=self.ver_noticia) #Botón para ver la noticia
-        self.ver_noticia_btn.pack(pady=10)  
+        self.ver_noticia_btn = tk.Button(ventana, text="Ver Noticia", command=self.ver_noticia, **estilo_boton) #Botón para ver la noticia
+        self.ver_noticia_btn.pack(pady=10)
 
-        self.salir_btn = tk.Button(ventana, text="Salir", font=("Arial", 14), command=ventana) #Botón para salir de la aplicación
+        self.salir_btn = tk.Button(ventana, text="Salir", command=ventana, **estilo_boton) #Botón para salir de la aplicación
         self.salir_btn.pack()
 
         self.noticias = []
@@ -40,9 +60,6 @@ class NoticiApp: #Creamos la clase NoticiApp
         pass
 
     def ver_noticia(self):
-        pass
-
-    def salir(self):
         pass
 
 if __name__ == "__main__":
